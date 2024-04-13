6028eb8750a76743190bb63488e8a17bb5595cd7
Tejas Ghutukade <tejas.ghutukade@vista.com>
2024-04-13 14:50:31 -0400

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import MainContent from './components/MainContent';
+import About from './components/About';
+import Contact from './components/Contact';
+import Meditateathome from './components/Meditateathome';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavBar/>
+        <Routes>
+        <Route path="/" element={<MainContent              
+              videos={[
+                {
+                  id: 'YdEubbf2SO8',
+                  title: 'Quick and Easy Meditations',
+                  thumbnail: 'far_left.jpg',
+                  isHiddenInMobile: false,
+                },
+                {
+                  id: 'Rrshyqs1czU',
+                  title: 'Meditation for Stress Free Career',
+                  thumbnail: 'hqdefault_new.jpg',
+                  isHiddenInMobile: true,
+                },
+                {
+                  id: 'RRO_0Aptwts',
+                  title: 'How to Meditate for Beginners',
+                  thumbnail: 'slight_left.jpg',
+                  isHiddenInMobile: false,
+                },
+                {
+                  id: 'UvjwfMsTDIc',
+                  title: 'Life of founder: H.H. Shri Mataji Nirmala Devi',
+                  thumbnail: 'image.png',
+                  isHiddenInMobile: false,
+                },
+              ]}/>} />
+        <Route path="/home" element={<MainContent 
+              featuredVideo={{
+                id: 'YdEubbf2SO8',
+                title: 'How to Meditate - A Beginner\'s Guide',
+                thumbnail: 'https://i.ytimg.com/vi/YdEubbf2SO8/mqdefault.jpg',
+              }}
+              videos={[
+                {
+                  id: 'YdEubbf2SO8',
+                  title: 'Quick and Easy Meditations',
+                  thumbnail: 'far_left.jpg',
+                },
+                {
+                  id: 'Rrshyqs1czU',
+                  title: 'Meditation for Stress Free Career',
+                  thumbnail: 'hqdefault_new.jpg',
+                },
+                {
+                  id: 'RRO_0Aptwts',
+                  title: 'How to Meditate for Beginners',
+                  thumbnail: 'slight_left.jpg',
+                },
+                {
+                  id: 'UvjwfMsTDIc',
+                  title: 'Life of founder: H.H. Shri Mataji Nirmala Devi',
+                  thumbnail: 'image.png',
+                },
+              ]}/>} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/meditateathome" element={<Meditateathome />} />
+        </Routes>        
+        <Footer/>
+      </Router>
     </div>
   );
 }
+
+
 
 export default App;
